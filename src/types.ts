30a79387533f57205574dd04d6bd4b9b4367024b
Maxim Kott <maximkott@gmail.com>
2020-12-02 17:14:04 +0100

    Update dependencies

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import { ObservableForm } from "@bytesoftio/form"
 
-export type UseForm = <TValue extends object = any, TResult extends object = any>(initialValue: FormInitializer<TValue, TResult>) => ObservableForm<TValue, TResult>
+export type UseForm = <TValue extends object = any, TResult = any>(initialValue: FormInitializer<TValue, TResult>) => ObservableForm<TValue, TResult>
 
 export type CreateFormBinder = (form: ObservableForm) => ObservableFormBinder
 
@@ -13,7 +13,7 @@ export type InputBinding = { onChange: (e) => void, value: any, name: string }
 export type SelectBinding = { onChange: (e) => void, value: any, name: string }
 export type CheckboxBinding = { onChange: (e) => void, checked: any, name: string }
 export type RadioBinding = { onChange: (e) => void, checked: any, name: string }
-export type FormInitializer<TValue extends object, TResult extends object> = (() => ObservableForm<TValue, TResult>) | ObservableForm<TValue, TResult>
+export type FormInitializer<TValue extends object, TResult> = (() => ObservableForm<TValue, TResult>) | ObservableForm<TValue, TResult>
 
 export interface ObservableFormBinder {
   form(options?: Partial<FormBindingOptions>): SubmitBinding
